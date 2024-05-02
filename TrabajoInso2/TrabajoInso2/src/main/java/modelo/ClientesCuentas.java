36d5e63980bda6dd3ac358eab56ca44c18ac6477
pablodelcastillobarrio <72197031+pablodelcastillobarrio@users.noreply.github.com>
2024-05-02 17:28:17 +0200

    ClientesCuentas y CuentasPromociones

---
+++ TrabajoInso2/TrabajoInso2/src/main/java/modelo/ClientesCuentas.java
@@ -0,0 +1,94 @@
+package modelo;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import modelo.Clientes;
+import modelo.Cuentas;
+
+@Entity
+@Table(name = "clientesCuentas")
+public class ClientesCuentas implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idClientesCuentas")
+    private Integer idClientesCuentas;
+
+    @ManyToOne
+    @JoinColumn(name = "Cuentas_idCuenta", referencedColumnName = "idCuenta", nullable = false)
+    private Cuentas cuentas;
+
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name = "Clientes_idCliente", referencedColumnName = "idCliente", nullable = false),
+            @JoinColumn(name = "Clientes_Usuarios_idUsuario", referencedColumnName = "Usuarios_idUsuario", nullable = false)
+    })
+    private Clientes clientes;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 19 * hash + Objects.hashCode(this.idClientesCuentas);
+        hash = 19 * hash + Objects.hashCode(this.cuentas);
+        hash = 19 * hash + Objects.hashCode(this.clientes);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ClientesCuentas other = (ClientesCuentas) obj;
+        if (!Objects.equals(this.idClientesCuentas, other.idClientesCuentas)) {
+            return false;
+        }
+        if (!Objects.equals(this.cuentas, other.cuentas)) {
+            return false;
+        }
+        if (!Objects.equals(this.clientes, other.clientes)) {
+            return false;
+        }
+        return true;
+    }
+
+    public void setIdClientesCuentas(Integer idClientesCuentas) {
+        this.idClientesCuentas = idClientesCuentas;
+    }
+
+    public void setCuentas(Cuentas cuentas) {
+        this.cuentas = cuentas;
+    }
+
+    public void setClientes(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    public Integer getIdClientesCuentas() {
+        return idClientesCuentas;
+    }
+
+    public Cuentas getCuentas() {
+        return cuentas;
+    }
+
+    public Clientes getClientes() {
+        return clientes;
+    }
+}
---
+++ TrabajoInso2/TrabajoInso2/src/main/java/modelo/CuentasPromociones.java
@@ -0,0 +1,90 @@
+package modelo;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import modelo.Cuentas;
+import modelo.Promociones;
+
+@Entity
+@Table(name = "cuentasPromociones", schema = "Banco")
+public class CuentasPromociones implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCuentasPromociones")
+    private Integer idCuentasPromociones;
+
+    @ManyToOne
+    @JoinColumn(name = "Promociones_idPromociones", referencedColumnName = "idPromociones", nullable = false)
+    private Promociones promociones;
+
+    @ManyToOne
+    @JoinColumn(name = "Cuentas_idCuenta", referencedColumnName = "idCuenta", nullable = false)
+    private Cuentas cuentas;
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + Objects.hashCode(this.idCuentasPromociones);
+        hash = 17 * hash + Objects.hashCode(this.promociones);
+        hash = 17 * hash + Objects.hashCode(this.cuentas);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CuentasPromociones other = (CuentasPromociones) obj;
+        if (!Objects.equals(this.idCuentasPromociones, other.idCuentasPromociones)) {
+            return false;
+        }
+        if (!Objects.equals(this.promociones, other.promociones)) {
+            return false;
+        }
+        if (!Objects.equals(this.cuentas, other.cuentas)) {
+            return false;
+        }
+        return true;
+    }
+
+    public Integer getIdCuentasPromociones() {
+        return idCuentasPromociones;
+    }
+
+    public void setIdCuentasPromociones(Integer idCuentasPromociones) {
+        this.idCuentasPromociones = idCuentasPromociones;
+    }
+
+    public Promociones getPromociones() {
+        return promociones;
+    }
+
+    public void setPromociones(Promociones promociones) {
+        this.promociones = promociones;
+    }
+
+    public Cuentas getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(Cuentas cuentas) {
+        this.cuentas = cuentas;
+    }
+}
